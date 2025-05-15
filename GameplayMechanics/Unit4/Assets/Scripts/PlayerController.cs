8a971b473bb97bd26d2563abb2202a741b9c094f
TheDivineNight <bedoyad@roverkids.org>
2025-05-15 14:20:00 -0400

    Lesson 4.3 Done

--- GameplayMechanics/Unit4/Assets/Scripts/PlayerController.cs
+++ GameplayMechanics/Unit4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private GameObject focalPoint;
     private Rigidbody playerRb;
+    private float powerUpStrength = 15;
     public float speed = 5.0f;
+    public bool hasPowerUp = false;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +24,37 @@ public class PlayerController : MonoBehaviour
         float forwardImpt = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardImpt * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCoundownRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCoundownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with: " + collision.gameObject.name + "with powerup set to" + hasPowerUp);
+        }
     }
 }
